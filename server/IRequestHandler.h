331b30cd51e98b594f379936a5148576394c9961
ben :) <carmonben@gmail.com>
2022-11-22 23:11:47 +0200

    fixed a bug

--- server/IRequestHandler.h
+++ server/IRequestHandler.h
@@ -3,6 +3,7 @@
 #include "HandlerStructs.h"
 
 class IRequestHandler {
+public:
     virtual bool isRequestRelevant(RequestInfo request) = 0;
     virtual RequestResult handleRequest(RequestInfo request) = 0;
 
