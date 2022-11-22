9fb56a67236b3359fc6ce8f48905d7a26eb178f0
ben :) <carmonben@gmail.com>
2022-11-22 23:03:09 +0200

    added an empty request handler class

---
+++ server/RequestHandler.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_REQUESTHANDLER_H
+#define SERVER_REQUESTHANDLER_H
+
+#include "IRequestHandler.h"
+class RequestHandler : public IRequestHandler{
+public:
+    RequestHandler() = default;
+    ~RequestHandler();
+    bool isRequestRelevant(RequestInfo request) override;
+    RequestResult handleRequest(RequestInfo request) override;
+private:
+    //functions such as login signout and route request
+};
+
+
+#endif //SERVER_REQUESTHANDLER_H
