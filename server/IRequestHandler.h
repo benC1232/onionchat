2be2ff2689c46fd168f83165019f7ebb98dec353
ben :) <carmonben@gmail.com>
2022-11-22 23:03:39 +0200

    added virtual functions to the IRequestHandler interface

--- server/IRequestHandler.h
+++ server/IRequestHandler.h
@@ -1,8 +1,10 @@
 #ifndef SERVER_IREQUESTHANDLER_H
 #define SERVER_IREQUESTHANDLER_H
-
+#include "HandlerStructs.h"
 
 class IRequestHandler {
+    virtual bool isRequestRelevant(RequestInfo request) = 0;
+    virtual RequestResult handleRequest(RequestInfo request) = 0;
 
 };
 
