79e9200fac8b543f931a49e0a18a77a12320359b
ben :) <carmonben@gmail.com>
2022-12-03 17:23:01 +0200

    added a simple python server

--- client/testing/router_protocol.py
+++ client/testing/router_protocol.py
@@ -1,1 +1,14 @@
-#this script analyzes requests from the packet fowarding protocol
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            # analyze requests
+            conn.sendall(data)
