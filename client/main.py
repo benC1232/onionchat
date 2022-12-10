cf19eb06b5a338d544799b8b886b072aa4a81b41
ben :) <carmonben@gmail.com>
2022-12-10 16:38:32 +0200

    added a routing feature to the client

--- client/main.py
+++ client/main.py
@@ -1,7 +1,16 @@
 import server_comm
+import socket
+import routing
+import json
 def main():
-    server_comm.get_path()
-
+    nodes = server_comm.get_path()
+    with open("settings.json", "r") as json_file:
+        dest_settings = json.load(json_file)['dest_settings']
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((nodes[0].ip, nodes[0].port))
+        while True:
+            s.sendall(routing.build_request(nodes, (dest_settings['dest_ip'],dest_settings['dest_port']), input("enter message:")))
+            data = s.recv(1024)
 
 if __name__ == '__main__':
     main()
---
+++ client/routing.py
@@ -0,0 +1,13 @@
+import socket
+import server_comm
+
+def build_request(nodes, dest, content):
+    msg = bytes()
+    for node in nodes[1:]:
+        ip_bytes = socket.inet_aton(node.ip)
+        port_bytes = node.port.to_bytes(2,'big')
+        msg+=ip_bytes+port_bytes
+    dest_bytes = socket.inet_aton(dest[0])+dest[1].to_bytes(2,'big')
+    msg = len(content).to_bytes(4, 'big') + msg + dest_bytes + content.decode()
+    return msg
+
