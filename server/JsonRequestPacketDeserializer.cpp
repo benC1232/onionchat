6420b2f7bf4e1c0a5fe19073455e5959511ad830
ben :) <carmonben@gmail.com>
2022-12-13 23:04:55 +0200

    finished a deserializer for the ip data response

--- server/JsonRequestPacketDeserializer.cpp
+++ server/JsonRequestPacketDeserializer.cpp
@@ -19,4 +19,21 @@ GetRouteRequest JsonRequestPacketDeserializer::deserializeGetRouteRequest(Buffer
     //---------------
 }
 
+GetIpData JsonRequestPacketDeserializer::deserializeIpData(std::string buffer) {
+    nlohmann::json jsonObject = nlohmann::json::parse(buffer);
+    if(jsonObject["status"] == "fail")
+    {
+        throw std::runtime_error("api request failed");
+    }
+    GetIpData data;
+    data.ip = jsonObject["query"];
+    data.continent = jsonObject["continent"];
+    data.country = jsonObject["country"];
+    data.regionName = jsonObject["regionName"];
+    data.city = jsonObject["city"];
+    data.isp = jsonObject["isp"];
+
+    return data;
+}
+
 
