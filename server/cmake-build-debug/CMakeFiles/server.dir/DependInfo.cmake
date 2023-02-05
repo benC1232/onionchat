c93001ddf5c02f112932817b02d051cc391c496e
Ori Meiri <orimeiri05@gmail.com>
2023-02-05 13:24:22 +0200

    commit

---
+++ server/cmake-build-debug/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/sqlite3.c" "CMakeFiles/server.dir/sqlite3.c.o" "gcc" "CMakeFiles/server.dir/sqlite3.c.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/APICommunicator.cpp" "CMakeFiles/server.dir/APICommunicator.cpp.o" "gcc" "CMakeFiles/server.dir/APICommunicator.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/Communicator.cpp" "CMakeFiles/server.dir/Communicator.cpp.o" "gcc" "CMakeFiles/server.dir/Communicator.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/IDataBase.cpp" "CMakeFiles/server.dir/IDataBase.cpp.o" "gcc" "CMakeFiles/server.dir/IDataBase.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/IRequestHandler.cpp" "CMakeFiles/server.dir/IRequestHandler.cpp.o" "gcc" "CMakeFiles/server.dir/IRequestHandler.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/JsonRequestPacketDeserializer.cpp" "CMakeFiles/server.dir/JsonRequestPacketDeserializer.cpp.o" "gcc" "CMakeFiles/server.dir/JsonRequestPacketDeserializer.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/JsonResponsePacketSerializer.cpp" "CMakeFiles/server.dir/JsonResponsePacketSerializer.cpp.o" "gcc" "CMakeFiles/server.dir/JsonResponsePacketSerializer.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/LoginManager.cpp" "CMakeFiles/server.dir/LoginManager.cpp.o" "gcc" "CMakeFiles/server.dir/LoginManager.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/NodeRequestHandler.cpp" "CMakeFiles/server.dir/NodeRequestHandler.cpp.o" "gcc" "CMakeFiles/server.dir/NodeRequestHandler.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/RequestHandler.cpp" "CMakeFiles/server.dir/RequestHandler.cpp.o" "gcc" "CMakeFiles/server.dir/RequestHandler.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/RequestHandlerFactory.cpp" "CMakeFiles/server.dir/RequestHandlerFactory.cpp.o" "gcc" "CMakeFiles/server.dir/RequestHandlerFactory.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/Server.cpp" "CMakeFiles/server.dir/Server.cpp.o" "gcc" "CMakeFiles/server.dir/Server.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/SqliteDataBase.cpp" "CMakeFiles/server.dir/SqliteDataBase.cpp.o" "gcc" "CMakeFiles/server.dir/SqliteDataBase.cpp.o.d"
+  "/mnt/c/Magshimim/ThirdYear/haifa-1204-onionchat/server/main.cpp" "CMakeFiles/server.dir/main.cpp.o" "gcc" "CMakeFiles/server.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/APICommunicator.cpp.o"
+  "CMakeFiles/server.dir/APICommunicator.cpp.o.d"
+  "CMakeFiles/server.dir/Communicator.cpp.o"
+  "CMakeFiles/server.dir/Communicator.cpp.o.d"
+  "CMakeFiles/server.dir/IDataBase.cpp.o"
+  "CMakeFiles/server.dir/IDataBase.cpp.o.d"
+  "CMakeFiles/server.dir/IRequestHandler.cpp.o"
+  "CMakeFiles/server.dir/IRequestHandler.cpp.o.d"
+  "CMakeFiles/server.dir/JsonRequestPacketDeserializer.cpp.o"
+  "CMakeFiles/server.dir/JsonRequestPacketDeserializer.cpp.o.d"
+  "CMakeFiles/server.dir/JsonResponsePacketSerializer.cpp.o"
+  "CMakeFiles/server.dir/JsonResponsePacketSerializer.cpp.o.d"
+  "CMakeFiles/server.dir/LoginManager.cpp.o"
+  "CMakeFiles/server.dir/LoginManager.cpp.o.d"
+  "CMakeFiles/server.dir/NodeRequestHandler.cpp.o"
+  "CMakeFiles/server.dir/NodeRequestHandler.cpp.o.d"
+  "CMakeFiles/server.dir/RequestHandler.cpp.o"
+  "CMakeFiles/server.dir/RequestHandler.cpp.o.d"
+  "CMakeFiles/server.dir/RequestHandlerFactory.cpp.o"
+  "CMakeFiles/server.dir/RequestHandlerFactory.cpp.o.d"
+  "CMakeFiles/server.dir/Server.cpp.o"
+  "CMakeFiles/server.dir/Server.cpp.o.d"
+  "CMakeFiles/server.dir/SqliteDataBase.cpp.o"
+  "CMakeFiles/server.dir/SqliteDataBase.cpp.o.d"
+  "CMakeFiles/server.dir/main.cpp.o"
+  "CMakeFiles/server.dir/main.cpp.o.d"
+  "CMakeFiles/server.dir/sqlite3.c.o"
+  "CMakeFiles/server.dir/sqlite3.c.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
